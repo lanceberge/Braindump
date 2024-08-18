10810ef4db4ec4fea49d3c4c3d9e933ef1a6bb1e
lanceberge <bergeron.lance6@gmail.com>
2024-08-18 19:57:00 -0400

    render s3 files

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,15 @@
+import { s3 } from '$lib/aws/s3Client'
+import { ListObjectsV2Command } from '@aws-sdk/client-s3'
+import type { PageServerLoad } from './$types'
+
+export const load: PageServerLoad = async () => {
+  const command = new ListObjectsV2Command({
+    Bucket: 'braindump-bucket'
+  })
+
+  const response = await s3.send(command)
+
+  return {
+    files: response.Contents?.map((item) => item.Key) ?? []
+  }
+}
