2d31927c6627a3083bba7f7e064d759cb4edc539
lanceberge <bergeron.lance6@gmail.com>
2024-08-24 14:30:17 -0400

    added tailwind, basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
   export let data: PageData
 </script>
 
+<!-- TODO can I just use the exported field -->
 {#each data.filenamesAndFilePrefixes as [filename, filePrefix]}
   <li><a href="/braindump/{filename}">{filePrefix}</a></li>
 {/each}
