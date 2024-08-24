244d85fda201425bfeadc2f3d269e145180ddf87
lanceberge <bergeron.lance6@gmail.com>
2024-08-24 15:01:23 -0400

    main page in div container

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
- import '../app.css'
+  import '../app.css'
 </script>
 
-<slot />
+<main>
+  <div class="main-page">
+    <slot />
+  </div>
+</main>
+
+<style>
+  .main-page {
+    max-width: 800px; /* Set the maximum width you want */
+    margin: 0 auto; /* Center the content */
+    padding: 1rem; /* Add padding around the content */
+  }
+</style>
