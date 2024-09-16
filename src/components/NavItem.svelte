3b490bb01b38295a352909de84e521700aaa273f
lanceberge <bergeron.lance6@gmail.com>
2024-09-16 18:12:06 -0400

    add NavItem component

---
+++ src/components/NavItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+
+  export let href: string
+  export let name: string
+
+  $: isActive = $page.url.pathname === href
+</script>
+
+<li>
+  <a
+    {href}
+    class="px-3 py-2 rounded-md text-sm font-medium transition-colors duration-150 ease-in-out"
+    class:text-blue-700={isActive}
+    class:bg-blue-100={isActive}
+    class:text-gray-700={!isActive}
+    class:hover:text-gray-900={!isActive}
+    class:hover:bg-gray-100={!isActive}
+  >
+    {name}
+  </a>
+</li>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,21 @@
 <script lang="ts">
   import '../app.css'
+  import NavItem from '../components/NavItem.svelte'
 </script>
 
 <nav class="bg-gray-100 p-4">
   <div class="max-w-3xl mx-auto">
-    <ul class="flex space-x-4">
-      <li>
-        <a
-          href="/"
-          class="text-gray-700 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">Home</a
-        >
-      </li>
+    <ul class="flex space-x-1">
+      <NavItem href="/" name="Home" />
+      <NavItem href="/braindump" name="Braindump" />
+      <NavItem href="/projects" name="Projects" />
+      <NavItem href="/blog" name="Blog" />
     </ul>
   </div>
 </nav>
 
 <main>
-  <div class="max-w-3xl mx-auto p-4">
+  <div>
     <slot />
   </div>
 </main>
-
-<style>
-  .main-page {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 1rem;
-  }
-</style>
