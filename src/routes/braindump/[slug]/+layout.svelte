5d6dad7c075167f5c103ebef967dddf34f309b67
lanceberge <bergeron.lance6@gmail.com>
2024-09-17 19:38:55 -0400

    move hljs to +layout

---
+++ src/routes/braindump/[slug]/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { browser } from '$app/environment'
+  import hljs from 'highlight.js/lib/common'
+  import { afterUpdate } from 'svelte'
+
+  afterUpdate(() => {
+    if (browser) {
+      const contentDiv = document.querySelector('.loaded-content')
+      if (contentDiv) {
+        contentDiv.querySelectorAll('pre code').forEach((block) => {
+          hljs.highlightElement(block as HTMLElement)
+        })
+      }
+    }
+  })
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/styles/xcode.min.css"
+  />
+</svelte:head>
+
+<slot />
--- src/routes/braindump/[slug]/+page.svelte
+++ src/routes/braindump/[slug]/+page.svelte
@@ -1,25 +1,14 @@
 <script lang="ts">
   import type { PageData } from './$types'
-  import { afterUpdate } from 'svelte'
-  import hljs from 'highlight.js/lib/common'
 
   export let data: PageData
 
   afterUpdate(() => {
-    const contentDiv = document.querySelector('.loaded-content')
-    if (contentDiv) {
-      contentDiv.querySelectorAll('pre code').forEach((block) => {
-        hljs.highlightElement(block as HTMLElement)
-      })
     }
   })
 </script>
 
 <svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/styles/xcode.min.css"
-  />
   <title>{data.filePrefix}</title>
 </svelte:head>
 
