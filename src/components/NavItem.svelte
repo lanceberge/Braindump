f82e75e4124f60d2b3d652d5b34e5c33128a8e4f
lanceberge <bergeron.lance6@gmail.com>
2024-09-22 17:47:44 -0400

    update navitem, add path alias for components

--- src/components/NavItem.svelte
+++ src/components/NavItem.svelte
@@ -10,12 +10,14 @@
 <li>
   <a
     {href}
-    class="px-3 py-2 rounded-md text-sm font-medium transition-colors duration-150 ease-in-out"
+    class="px-3 py-2 rounded-md font-medium transition-colors duration-150 ease-in-out
+           text-md block"
     class:text-blue-700={isActive}
     class:bg-blue-100={isActive}
     class:text-gray-700={!isActive}
     class:hover:text-gray-900={!isActive}
-    class:hover:bg-gray-100={!isActive}
+    class:hover:bg-gray-200={!isActive}
+    on:click
   >
     {name}
   </a>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import path from 'path'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    alias: {
+      $components: path.resolve('./src/components')
+    }
   },
   preprocess: vitePreprocess()
 }
