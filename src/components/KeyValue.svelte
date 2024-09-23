df5270b6c702fad6ffbf6a297ed036d3c968f8e0
lanceberge <bergeron.lance6@gmail.com>
2024-09-23 18:48:29 -0400

    delete unused

--- src/components/KeyValue.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  export let key: string
-  export let value: string
-</script>
-
-<p class="text-base flex">
-  <span class="font-bold mr-1 flex-shrink-0">{key}:</span>
-  <span>{value}</span>
-</p>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,6 @@
 <script lang="ts">
   import '../app.css'
-  import NavItem from '$components/NavItem.svelte'
 </script>
-
-<!-- Top Nav Bar -->
-<nav class="bg-gray-100 px-4 py-2">
-  <div class="max-w-3xl mx-auto">
-    <ul class="flex space-x-1">
-      <NavItem href="/" name="Home" />
-      <NavItem href="/braindump" name="Braindump" />
-      <NavItem href="/projects" name="Projects" />
-    </ul>
-  </div>
-</nav>
 
 <main>
   <slot />
